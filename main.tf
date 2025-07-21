918d16506388917d15a1be232aa85dfcb5b2dda3
Jeremy Belcher <radiobelcher@gmail.com>
2025-07-21 18:33:01 -0400

    eve-ng fix

--- main.tf
+++ main.tf
@@ -10,34 +10,6 @@ resource "google_compute_instance" "eve_ng" {
   name         = "eve-ng-vm"
   machine_type = "n2-standard-16"
   zone         = var.zone
-  metadata_startup_script = file("eve_ng_startup.sh")
-
-
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2204-lts"
-      size  = 200
-      type  = "pd-ssd"
-    }
-  }
-
-  network_interface {
-    network       = "default"
-    access_config {}
-  }
-
-  metadata = {
-    enable-oslogin = "TRUE"
-  }
-
-  tags = ["http-server", "https-server", "eve-ng"]
-}
-
-
-resource "google_compute_instance" "eve_ng" {
-  name         = "eve-ng-vm"
-  machine_type = "n2-standard-16"
-  zone         = var.zone
 
   metadata_startup_script = file("eve_ng_startup_autoinstall.sh")
 
@@ -60,3 +32,4 @@ resource "google_compute_instance" "eve_ng" {
 
   tags = ["http-server", "https-server", "eve-ng"]
 }
+
