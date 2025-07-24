6798efbb5f28b9204fdc928eef0a5cf40df429c5
radiobelcher <40397603+radiobelcher@users.noreply.github.com>
2025-07-23 21:28:58 -0400

    Update main.tf
    redeploy with 20.04

--- main.tf
+++ main.tf
@@ -1,35 +1,22 @@
-provider "google" {
-  credentials = base64decode(var.google_credentials_b64)
-  project     = var.project_id
-  region      = var.region
-  zone        = var.zone
-}
-
-
 resource "google_compute_instance" "eve_ng" {
   name         = "eve-ng-vm"
-  machine_type = "n2-standard-16"
-  zone         = var.zone
-
-  metadata_startup_script = file("eve_ng_startup_autoinstall.sh")
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2204-lts"
-      size  = 200
-      type  = "pd-ssd"
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"  # ✅ Change here
+      size  = 50
     }
   }
 
   network_interface {
-    network       = "default"
-    access_config {}
+    network = "default"
+
+    access_config {
+      # This gives the instance a public IP
+    }
   }
 
-  metadata = {
-    enable-oslogin = "TRUE"
-  }
-
-  tags = ["http-server", "https-server", "eve-ng"]
+  metadata_startup_script = file("eve_ng_startup_autoinstall.sh")  # or null if you're manually installing
 }
-
