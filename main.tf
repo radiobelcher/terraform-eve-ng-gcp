5ee2b51cd62928eb732cc2df1b42cb6ddcd05579
radiobelcher <40397603+radiobelcher@users.noreply.github.com>
2025-07-23 22:41:26 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,42 @@
-resource "google_compute_instance" "eve_ng" {
-  name         = "eve-ng-vm"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
-
-boot_disk {
-  initialize_params {
-    image  = "ubuntu-os-cloud/ubuntu-2204-lts"
-    size   = 40
-    type   = "pd-standard"
-  }
-}
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      # This gives the instance a public IP
-    }
-  }
-
-  metadata_startup_script = file("eve_ng_startup_autoinstall.sh")  # or null if you're manually installing
-}
-
+# main.tf
 provider "google" {
   credentials = base64decode(var.google_credentials_b64)
   project     = var.project_id
   region      = var.region
   zone        = var.zone
+}
+
+resource "google_compute_instance" "eve_ng" {
+  name         = "eve-ng-vm"
+  machine_type = "e2-medium"
+  zone         = var.zone
+
+  boot_disk {
+    auto_delete = true
+    initialize_params {
+      image = var.custom_image_name
+      size  = 50
+      type  = "pd-standard"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  tags = ["allow-ssh", "allow-http", "allow-https"]
+}
+
+resource "google_compute_firewall" "allow_external" {
+  name    = "allow-ssh-http-https"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "443"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["allow-ssh", "allow-http", "allow-https"]
 }
